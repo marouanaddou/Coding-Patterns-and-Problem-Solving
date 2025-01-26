eea3d72287214a6cc4c3dce1f64c2d4ef2169e94
marouanaddou <105064179+marouanaddou@users.noreply.github.com>
2025-01-26 18:15:56 +0100

    Optimization of the second approach ( remove_duplicate )

--- TwoPointer&&SlidingWindow/RemoveDuplicates/RemoveDuplicates.cpp
+++ TwoPointer&&SlidingWindow/RemoveDuplicates/RemoveDuplicates.cpp
@@ -6,51 +6,69 @@
 
 using namespace std;
 
- // first approache
-// int main() {
-// 	// your code goes here
-//     int t , size, item;
-//     cin >> t;
-//     for ( int i = 0; i < t; i++ ) {
-//         set<int> num;
-//         cin >> size;
-//         for ( int j = 0; j < size; j++ ) {
-//             cin >> item;
-//             num.insert(item);
-//         }
-        
-//         std::cout << num.size() << std::endl;
-        
-//         for ( set<int>::iterator it = num.begin(); it != num.end(); it++ ) {
-//             std::cout << *it << " ";
-//         }
-//         std::cout << std::endl;
-//     }
-// }
+    // first approache
+        // int main() {
+        // 	// your code goes here
+        //     int t , size, item;
+        //     cin >> t;
+        //     for ( int i = 0; i < t; i++ ) {
+        //         set<int> num;
+        //         cin >> size;
+        //         for ( int j = 0; j < size; j++ ) {
+        //             cin >> item;
+        //             num.insert(item);
+        //         }
+        //         std::cout << num.size() << std::endl;
+        //         for ( set<int>::iterator it = num.begin(); it != num.end(); it++ ) {
+        //             std::cout << *it << " ";
+        //         }
+        //         std::cout << std::endl;
+        //     }
+        // }
 
     // second approache
-void remove_duplicate( std::vector<int> array , int &size ) {
-    std::vector<int> distinctElements;
-    distinctElements.push_back( array[0] );
-    for ( int i = 1; i  < size; i++ ) {
-        if ( array[i] != array[i - 1] ) distinctElements.push_back( array[i] );
-    }
-    size = distinctElements.size();
-    std::cout << size << std::endl;
-    for ( int i = 0; i < size ; i++ ) {
-        std::cout << distinctElements[i] << " ";
-    }
-    std::cout << std::endl;
-}
-int main() {
-    int t , item;
-    cin >> t;
-    while ( t-- ) {
-        cin >> item;
-        std::vector<int> array(item);
-        for ( int i = 0; i < item; i++ ) {
-            cin >> array[i];
+
+            // void remove_duplicate( std::vector<int> array , int &size ) {
+            //     std::vector<int> distinctElements;
+            //     distinctElements.push_back( array[0] );
+            //     for ( int i = 1; i  < size; i++ ) {
+            //         if ( array[i] != array[i - 1] ) distinctElements.push_back( array[i] );
+            //     }
+            //     size = distinctElements.size();
+            //     std::cout << size << std::endl;
+            //     for ( int i = 0; i < size ; i++ ) {
+            //         std::cout << distinctElements[i] << " ";
+            //     }
+            //     std::cout << std::endl;
+            // }
+
+
+    // Optimization of the second approach ( remove_duplicate )
+
+            void remove_duplicate( std::vector<int> &array , int &size ) {
+                int j = 1;
+                for ( int i = 1; i < size; i++ ) {
+                    if ( array[i] != array[j - 1] ) {
+                        array[ j ] = array[i];
+                        j++;
+                    }
+                }
+                std::cout << j << std::endl;
+                for ( int i = 0; i < j ; i++ ) {
+                    std::cout << array[i] << " ";
+                }
+                std::cout << std::endl;
+            }
+    
+    int main() {
+        int t , item;
+        cin >> t;
+        while ( t-- ) {
+            cin >> item;
+            std::vector<int> array(item);
+            for ( int i = 0; i < item; i++ ) {
+                cin >> array[i];
+            }
+            remove_duplicate( array, item );
         }
-        remove_duplicate( array, item );
     }
-}
